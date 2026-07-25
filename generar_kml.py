023a26d4ae5f08b4077502d39bdcdc1ed64fee77
andreasolanorosique-lab <andrea.solano.rosique@gmail.com>
2026-07-25 21:24:47 +0200

    Update generar_kml.py

--- generar_kml.py
+++ generar_kml.py
@@ -1,1 +1,9 @@
-print("Proyecto Copernicus iniciado")
+import requests
+
+url = "https://effis.jrc.ec.europa.eu/"
+
+r = requests.get(url, timeout=30)
+
+print("Código HTTP:", r.status_code)
+print("Primeros 200 caracteres:")
+print(r.text[:200])
