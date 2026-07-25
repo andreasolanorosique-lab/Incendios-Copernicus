2c63c047b50e327a7c3dff15c34977bc90579821
andreasolanorosique-lab <andrea.solano.rosique@gmail.com>
2026-07-25 21:30:20 +0200

    Update generar_kml.py

--- generar_kml.py
+++ generar_kml.py
@@ -4,5 +4,6 @@ url = "https://maps.effis.emergency.copernicus.eu/effis"
 
 r = requests.get(url, timeout=30)
 
+print("URL final:", r.url)
 print("Estado:", r.status_code)
-print(r.text[:500])
+print("Tipo:", r.headers.get("content-type"))
